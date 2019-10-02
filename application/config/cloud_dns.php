b670119d8b60636f68240c79171f131d94839c1a
Liki Crus <likigoldenstar@gmail.com>
2019-10-03 00:35:53 +0800

    config updated.

--- application/config/cloud_dns.php
+++ application/config/cloud_dns.php
@@ -1,9 +1,9 @@
 <?php
 $config['cloud_dns'] = array(
 	'is_subuser' => false,
-	//'auth-id' => '3283', // for local
-	'auth-id' => '3296',
-	'auth-password' => 'eDZbR59.gwe87iL',
+	//'auth-id' => '', // for local
+	'auth-id' => '',
+	'auth-password' => '',
 	'sub-auth-user' => '',
 	'domain-name' => '',
 );
